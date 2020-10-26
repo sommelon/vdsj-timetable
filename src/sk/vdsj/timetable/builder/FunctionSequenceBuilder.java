1c412919b65f767909c3c51afc15a11cebf58d5b
Simeon Kralik <simeon.kralik@student.tuke.sk>
2020-10-26 12:02:16 +0100

    fix: sequence builder not ordered properly

--- src/sk/vdsj/timetable/builder/FunctionSequenceBuilder.java
+++ src/sk/vdsj/timetable/builder/FunctionSequenceBuilder.java
@@ -1,73 +1,65 @@
 package sk.vdsj.timetable.builder;
 
-import sk.vdsj.timetable.model.*;
+import sk.vdsj.timetable.model.Event;
+import sk.vdsj.timetable.model.Schedule;
+import sk.vdsj.timetable.model.Time;
+import sk.vdsj.timetable.model.Timetable;
 
-import java.time.DayOfWeek;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.List;
 
 public class FunctionSequenceBuilder {
-    private static String timetableProgramme;
-    private static String timetableSemester;
-    private static String timetableGrade;
+    private static Timetable timetable;
+    private static Schedule scheduleContext;
+    private static Event eventContext;
 
-    private static String scheduleTitle;
-    private static List<Schedule> schedules = new ArrayList<>();
+    private static ArrayList<Schedule> schedules = new ArrayList<>();
+    private static ArrayList<Event> events;
+    private static ArrayList<String> persons;
 
-    private static List<Event> events;
-    private static String eventType;
-    private static Time formattedTime;
-    private static String eventRoom;
-    private static String eventGroups;
-    private static String eventNote;
-
-    private static List<String> persons;
-
-    public static void timetable(String programme, String semester, String grade){
-        timetableProgramme = programme;
-        timetableSemester = semester;
-        timetableGrade = grade;
+    public static void timetable(String programme, String semester, String grade) {
+        timetable = new Timetable(programme, semester, grade, null);
     }
 
-    public static void schedule(String title){
-        if(scheduleTitle != null) createSchedule();
-        scheduleTitle = title;
+    public static void schedule(String title) {
+        addPreviousSchedule();
+        eventContext = null;
+        scheduleContext = new Schedule(title, null);
         events = new ArrayList<>();
     }
 
-    public static void event(String type, String time, String room, String groups, String note){
-        DayOfWeek day = DayOfWeek.valueOf(Days.valueOf(time.substring(0,time.indexOf(' '))).getKey());
-        LocalTime startTime = LocalTime.parse(time.substring(time.indexOf(' ') + 1, time.indexOf('-')));
-        LocalTime endTIme = LocalTime.parse(time.substring(time.indexOf('-') + 1));
-
-        if(eventType != null) createEvent();
-        eventType = type;
-        formattedTime = new Time(day, startTime, endTIme);
-        eventRoom = room;
-        eventGroups = groups;
-        eventNote = note;
-
+    public static void event(String type, String time, String room, String groups, String note) {
+        addPreviousEvent();
+        eventContext = new Event(type, Time.valueOf(time), room, groups, null, note);
         persons = new ArrayList<>();
     }
 
-    public static void person(String name){
+    public static void person(String name) {
         persons.add(name);
     }
 
+    private static void addPreviousSchedule() {
+        if (scheduleContext == null) {
+            return;
+        }
+
+        addPreviousEvent();
+        scheduleContext.setEvents(events.toArray(new Event[]{}));
+        schedules.add(scheduleContext);
+    }
+
+    private static void addPreviousEvent() {
+        if (eventContext == null) {
+            return;
+        }
+
+        eventContext.setPersons(persons.toArray(new String[]{}));
+        events.add(eventContext);
+    }
+
     public static Timetable getTimeTable() {
-        createEvent();
-        createSchedule();
-
-        return new Timetable(timetableProgramme, timetableSemester, timetableGrade, schedules.toArray(new Schedule[] {}));
-    }
-
-    private static void createSchedule() {
-        schedules.add(new Schedule(scheduleTitle, events.toArray(new Event[]{})));
-    }
-
-    private static void createEvent() {
-        events.add(new Event(eventType, formattedTime, eventRoom, eventGroups, persons.toArray(new String[]{}), eventNote));
+        addPreviousSchedule();
+        timetable.setSchedules(schedules.toArray(new Schedule[]{}));
+        return timetable;
     }
 
 }
