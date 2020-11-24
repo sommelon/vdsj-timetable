bff054413363b23072fadad45e8880e3be4e7341
Kirill Kalcickij <kirill.kalcickij@gmail.com>
2020-11-24 18:33:51 +0100

    visitor added methods

--- src/sk/vdsj/timetable/antlr4/grammar/GrammarParserVisitor.java
+++ src/sk/vdsj/timetable/antlr4/grammar/GrammarParserVisitor.java
@@ -2,12 +2,13 @@ package sk.vdsj.timetable.antlr4.grammar;
 
 import sk.vdsj.timetable.model.Event;
 import sk.vdsj.timetable.model.Schedule;
+import sk.vdsj.timetable.model.Time;
 import sk.vdsj.timetable.model.Timetable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class GrammarParserVisitor extends GrammarBaseVisitor {
+public class GrammarParserVisitor extends GrammarBaseVisitor<Void> {
     private Timetable timetable;
     private List<Schedule> schedules;
     private Schedule schedule;
@@ -20,89 +21,109 @@ public class GrammarParserVisitor extends GrammarBaseVisitor {
     }
 
     @Override
-    public Object visitTimetable(GrammarParser.TimetableContext ctx) {
+    public Void visitTimetable(GrammarParser.TimetableContext ctx) {
         timetable = new Timetable(null, null, null, null);
         schedules = new ArrayList<>();
+        events = new ArrayList<>();
         return super.visitTimetable(ctx);
     }
 
     @Override
-    public Object visitTimetableHeader(GrammarParser.TimetableHeaderContext ctx) {
-        return super.visitTimetableHeader(ctx);
-    }
-
-    @Override
-    public Object visitTimetableName(GrammarParser.TimetableNameContext ctx) {
+    public Void visitTimetableName(GrammarParser.TimetableNameContext ctx) {
+        String[] name = new String[ctx.getChildCount()];
+        for (int x = 0; x < ctx.getChildCount(); x++) {
+            name[x] = ctx.getChild(x).getText();
+        }
+        timetable.setProgramme(String.join(" ", name));
+        super.visitTimetableName(ctx);
         return super.visitTimetableName(ctx);
     }
 
     @Override
-    public Object visitTimetableYear(GrammarParser.TimetableYearContext ctx) {
+    public Void visitTimetableYear(GrammarParser.TimetableYearContext ctx) {
+        String halfYear = ctx.getChild(0).getText();
+        timetable.setSemester(halfYear + " " + ctx.getText().substring(halfYear.length()));
+        super.visitTimetableYear(ctx);
         return super.visitTimetableYear(ctx);
     }
 
     @Override
-    public Object visitTimetableGrade(GrammarParser.TimetableGradeContext ctx) {
+    public Void visitTimetableGrade(GrammarParser.TimetableGradeContext ctx) {
+        timetable.setGrade(ctx.getText());
         return super.visitTimetableGrade(ctx);
     }
 
     @Override
-    public Object visitSchedule(GrammarParser.ScheduleContext ctx) {
+    public Void visitSchedule(GrammarParser.ScheduleContext ctx) {
+        schedule = new Schedule(null , null);
+        schedules.add(schedule);
+        timetable.setSchedules(schedules.toArray(new Schedule[0]));
         return super.visitSchedule(ctx);
     }
 
     @Override
-    public Object visitScheduleName(GrammarParser.ScheduleNameContext ctx) {
+    public Void visitScheduleName(GrammarParser.ScheduleNameContext ctx) {
+        events = new ArrayList<>();
+        String[] name = new String[ctx.getChildCount()];
+        for (int x = 0; x < ctx.getChildCount(); x++) {
+            name[x] = ctx.getChild(x).getText();
+        }
+        schedule.setTitle(String.join(" ", name));
         return super.visitScheduleName(ctx);
     }
 
     @Override
-    public Object visitEvent(GrammarParser.EventContext ctx) {
+    public Void visitEvent(GrammarParser.EventContext ctx) {
+        event = new Event(null, null, null, null, null, null);
+        teachers = new ArrayList<>();
+        events.add(event);
+        schedule.setEvents(events.toArray(new Event[0]));
         return super.visitEvent(ctx);
     }
 
     @Override
-    public Object visitTime(GrammarParser.TimeContext ctx) {
+    public Void visitTime(GrammarParser.TimeContext ctx) {
+        String day = ctx.getChild(0).getText();
+        event.setTime(Time.valueOf(day + " " + ctx.getText().substring(day.length())));
         return super.visitTime(ctx);
     }
 
     @Override
-    public Object visitEventType(GrammarParser.EventTypeContext ctx) {
+    public Void visitEventType(GrammarParser.EventTypeContext ctx) {
+        event.setType(ctx.getText());
         return super.visitEventType(ctx);
     }
 
     @Override
-    public Object visitEventGroups(GrammarParser.EventGroupsContext ctx) {
+    public Void visitEventGroups(GrammarParser.EventGroupsContext ctx) {
+        event.setGroups(ctx.getText());
         return super.visitEventGroups(ctx);
     }
 
     @Override
-    public Object visitDay(GrammarParser.DayContext ctx) {
-        return super.visitDay(ctx);
-    }
-
-    @Override
-    public Object visitTime_from(GrammarParser.Time_fromContext ctx) {
-        return super.visitTime_from(ctx);
-    }
-
-    @Override
-    public Object visitTime_to(GrammarParser.Time_toContext ctx) {
-        return super.visitTime_to(ctx);
-    }
-
-    @Override
-    public Object visitRoom(GrammarParser.RoomContext ctx) {
+    public Void visitRoom(GrammarParser.RoomContext ctx) {
+        event.setLocation(ctx.getText());
         return super.visitRoom(ctx);
     }
 
     @Override
-    public Object visitTeacher(GrammarParser.TeacherContext ctx) {
+    public Void visitTeacher(GrammarParser.TeacherContext ctx) {
+        String[] name = new String[ctx.getChildCount()];
+        for (int x = 0; x < ctx.getChildCount(); x++) {
+            name[x] = ctx.getChild(x).getText();
+        }
+        teachers.add(String.join(" ", name));
+        event.setOrganisers(teachers.toArray(new String[0]));
         return super.visitTeacher(ctx);
     }
 
     @Override
-    public Object visitNote(GrammarParser.NoteContext ctx) {
+    public Void visitNote(GrammarParser.NoteContext ctx) {
+        String[] note = new String[ctx.getChildCount()];
+        for (int x = 0; x < ctx.getChildCount(); x++) {
+            note[x] = ctx.getChild(x).getText();
+        }
+        event.setNote(String.join(" ", note));
         return super.visitNote(ctx);
     }
 }
